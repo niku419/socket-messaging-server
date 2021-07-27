c16ef209ef370c3cf775f90a78110cc202d59d07
Allam Nikhil Sai <2019198@iiitdmj.ac.in>
2021-07-27 21:05:56 +0530

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,6 +3,7 @@ const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
 io.on("connection", socket => { 
   socket.on('message',({name, message}) =>{
+    console.log(message)
     io.emit('message',{name,message})
   })
 });
